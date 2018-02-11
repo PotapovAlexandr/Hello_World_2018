bcfa6d40437c3a159a98c47f39b7dd867ee43bd3
AnastasiiaRomaniuk <36264849+AnastasiiaRomaniuk@users.noreply.github.com>
2018-02-11 13:58:28 +0200

    Add files via upload

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+#include "nastya_function.h"
+
+int main()
+{
+	nastya_print();
+    return 0;
+}
+
---
+++ HelloWorld/HelloWorld/nastya_function.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+void nastya_print()
+{
+	cout << "nastya hello\n";
+}
